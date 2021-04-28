d3b31d1488c1056f5cf411f801d0508aa3ebbe25
monica1123 <shelly1123@hotmail.com>
2021-04-28 20:34:54 +0800

    Initial Commit

---
+++ shopping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shopping
+//
+//  Created by Monica Lo on 2021/4/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
